b25f7eae4f4dded9302cd932da9b2f4f1c53ba28
Bowen <bowen.sui@hotmail.com>
2019-04-06 17:03:24 +0200

    Finish part 19. Exhaustmap

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { of, concat, interval, merge } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+    selector: 'about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // this.p14_concatenation();
+        // this.p17_merge();
+        this.p10_unsubscription()
+    }
+
+    p14_concatenation() {
+        const source1$ = of(1, 2, 3);
+        const source2$ = of(4, 5, 6);
+        const source3$ = of(10, 11, 12);
+        const result$ = concat(source1$, source2$, source3$);
+        result$.subscribe(console.log);
+    }
+
+    p17_merge() {
+        const interval1$ = interval(1000);
+        const interval2$ = interval1$.pipe(map(val => 10 * val));
+        const result$ = merge(interval1$, interval2$);
+        result$.subscribe(console.log);
+    }
+
+    p10_unsubscription() {
+        const interval1$ = interval(1000);
+        const sub = interval1$.subscribe(console.log);
+        setTimeout(() => sub.unsubscribe(), 5000);
+    }
 
 }
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 import * as moment from 'moment';
-import {fromEvent} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
-import {fromPromise} from 'rxjs/internal-compatibility';
+import { fromEvent } from 'rxjs';
+import { concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap } from 'rxjs/operators';
+import { fromPromise } from 'rxjs/internal-compatibility';
 
 @Component({
     selector: 'course-dialog',
@@ -15,16 +15,16 @@ import {fromPromise} from 'rxjs/internal-compatibility';
 export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     form: FormGroup;
-    course:Course;
+    course: Course;
 
     @ViewChild('saveButton') saveButton: ElementRef;
 
-    @ViewChild('searchInput') searchInput : ElementRef;
+    @ViewChild('searchInput') searchInput: ElementRef;
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course ) {
+        @Inject(MAT_DIALOG_DATA) course: Course) {
 
         this.course = course;
 
@@ -32,25 +32,69 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
             description: [course.description, Validators.required],
             category: [course.category, Validators.required],
             releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
+            longDescription: [course.longDescription, Validators.required]
         });
 
     }
 
     ngOnInit() {
-
-
-
+        //this.p15_preSaveExample();
+        //this.p16_concatMap();
+        this.p18_mergeMap();
     }
 
+    p15_preSaveExample() {
+        this.form.valueChanges
+            .pipe(
+                filter(() => this.form.valid)
+            )
+            .subscribe(changes => {
+                const saveCourse$ = this.saveCourse(changes);
+                saveCourse$.subscribe();
+            });
+    }
 
+    p16_concatMap() {
+        this.form.valueChanges
+            .pipe(
+                filter(() => this.form.valid),
+                concatMap(changes => this.saveCourse(changes))
+            )
+            .subscribe();
+    }
+
+    p18_mergeMap() {
+        this.form.valueChanges
+            .pipe(
+                filter(() => this.form.valid),
+                mergeMap(changes => this.saveCourse(changes))
+            )
+            .subscribe();
+    }
+
+    p19_exhaustMap() {
+        fromEvent(this.saveButton.nativeElement, "click")
+        .pipe(
+            exhaustMap(() => this.saveCourse(this.form.value))
+        )
+        .subscribe();
+    }
+
+    saveCourse(changes) {
+        return fromPromise(
+            fetch(`/api/courses/${this.course.id}`, {
+                method: "PUT",
+                body: JSON.stringify(changes),
+                headers: {
+                    "content-type": "application/json"
+                }
+            })
+        );
+    }
 
     ngAfterViewInit() {
-
-
+        this.p19_exhaustMap();
     }
-
-
 
     close() {
         this.dialogRef.close();
