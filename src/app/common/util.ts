683a069937e153c81a9af7e9f65f92a85295a6b1
Bowen <bowen.sui@hotmail.com>
2019-04-06 15:50:19 +0200

    finish part 10, 11 and 12.
    Building Components with Rxjs Imperative Design vs Reactive Design.

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,2 +1,19 @@
+import { Observable } from "rxjs";
+
+export function createHttpObservable(url: string) {
+    return Observable.create(observer => {
+        fetch(url)
+            .then(response => {
+                return response.json();
+            })
+            .then(body => {
+                observer.next(body);
+                observer.complete();
+            })
+            .catch(error => {
+                observer.error(error);
+            })
+    });
+}
 
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
-
+import { Component, OnInit } from '@angular/core';
+import { Course } from "../model/course";
+import { interval, Observable, of, timer } from 'rxjs';
+import { catchError, delayWhen, map, retryWhen, shareReplay, tap } from 'rxjs/operators';
+import { createHttpObservable } from "../common/util";
 
 @Component({
     selector: 'home',
@@ -11,15 +11,61 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
+    beginnersCourses: Course[];
+    advancedCourses: Course[];
+
+    beginnersCourses$: Observable<Course[]>;
+    advancedCourses$: Observable<Course[]>;
 
     constructor() {
 
     }
 
     ngOnInit() {
+        this.p11_reactive_design();
+    }
 
+    p10_imperative_design() {
+        const http$ = createHttpObservable("/api/courses");
 
+        const courses$: Observable<Course[]> = http$
+            .pipe(
+                map(res => Object.values(res["payload"]))
+            );
 
+        courses$.subscribe(
+            courses => {
+                this.beginnersCourses = courses
+                    .filter(course => course.category === "BEGINNER");
+                this.advancedCourses = courses
+                    .filter(course => course.category === "ADVANCED");
+            },
+            () => { },
+            () => console.log("completed")
+        );
+    }
+
+    p11_reactive_design() {
+        const http$ = createHttpObservable("/api/courses");
+
+        const courses$: Observable<Course[]> = http$
+            .pipe(
+                tap(() => console.log("HTTP request executed")),
+                map(res => Object.values(res["payload"])),
+                shareReplay(),
+            );
+
+        this.beginnersCourses$ = courses$
+            .pipe(
+                map(courses => courses
+                    .filter(course => course.category === "BEGINNER"))
+            );
+
+        this.advancedCourses$ = courses$
+            .pipe(
+                map(courses => courses
+                    .filter(course => course.category === "ADVANCED"))
+            );
     }
 
 }
