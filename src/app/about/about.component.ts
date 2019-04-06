54be5a8414015ecb8b44bac1ef785276dbf427c2
Bowen <bowen.sui@hotmail.com>
2019-04-06 14:12:35 +0200

    Finish section 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {
     concat,
     fromEvent,
@@ -14,9 +14,8 @@ import {
     AsyncSubject,
     ReplaySubject
 } from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { delayWhen, filter, map, take, timeout } from 'rxjs/operators';
+import { createHttpObservable } from '../common/util';
 
 @Component({
     selector: 'about',
@@ -26,15 +25,70 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
+        // this.l01p06_rxjs_observable(); 
+        // this.l01p08_http_observable();
+        this.l02p01_map_operator();
+    }
 
+    l01p05_stream_chainning_problem() {
+        document.addEventListener("click", event => {
+            console.log(event);
+        });
+
+        let counter = 0;
+
+        setInterval(() => {
+            console.log(counter);
+            counter++;
+        }, 1000);
+
+        setTimeout(() => {
+            console.log("finished");
+        }, 3000);
+    }
+
+    l01p06_rxjs_observable() {
+        const intervals$: Observable<number> = interval(1000);
+
+        intervals$.subscribe(value => console.log(`stream 1 => ${value}`));
+        intervals$.subscribe(value => console.log(`stream 2 => ${value}`));
+
+        const click$ = fromEvent(document, "click");
+        click$.subscribe(event => console.log(event));
 
     }
 
+    l01p07_errors_completion_subscription() {
+        const click$ = fromEvent(document, "click");
+        click$.subscribe(
+            event => console.log(event),
+            error => console.log(error),
+            () => console.log("completed")
+        );
+    }
 
+    l01p08_http_observable() {
+        const http$ = createHttpObservable("/api/courses");
+
+        http$.subscribe(
+            courses => console.log(courses),
+            noop,
+            () => console.log("completed")
+        );
+    }
+
+    l02p01_map_operator() {
+        const http$ = createHttpObservable("/api/courses");
+
+        const courses$ = http$.pipe(
+            map(response => Object.values(response["payload"]))
+        );
+
+        courses$.subscribe(
+            courses => console.log(courses),
+            noop,
+            () => console.log("completed")
+        );
+    }
+    
 }
-
-
-
-
-
-
